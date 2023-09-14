f54006ea3f5bc41fc5627678edea96a4ea82ebd6
Misbah <142839155+misbahSyedd@users.noreply.github.com>
2023-09-14 14:48:29 -0500

    Comments added to simplegame.py

--- simplegame.py
+++ simplegame.py
@@ -1,5 +1,6 @@
 import random
 
+# Define ASCII art images for rock, paper, and scissors
 rock = '''
     _______
 ---'   ____)
@@ -26,19 +27,29 @@ scissors = '''
       (____)
 ---.__(___)
 '''
+
+# Store the images in a list for easy access
 game_images = [rock, paper, scissors]
 
-user_input = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
+# Get user input for their choice
+user_input = int(input("What do you choose? Type 0 for Rock, 1 for Paper, or 2 for Scissors.\n"))
+
+# Check if the user input is valid
 if user_input >= 3 or user_input < 0:
     print("You typed an invalid number, you lose!")
 else:
+    # Display the user's choice
+    print("You chose:")
     print(game_images[user_input])
 
+    # Generate a random choice for the computer
     computer_choice = random.randint(0, 2)
+    
+    # Display the computer's choice
     print("Computer chose:")
     print(game_images[computer_choice])
 
-
+    # Determine the game result based on choices
     if user_input == 0 and computer_choice == 2:
         print("You win!")
     elif computer_choice == 0 and user_input == 2:
